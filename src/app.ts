93398b34665054484766c86022bea7aab7c27061
Julliane <dipaulajulliane@gmail.com>
2026-03-10 20:23:24 -0300

    fix: fixing the register route from front-end

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import cookie from '@fastify/cookie'
+import fastifyCors from '@fastify/cors'
 import fastify from 'fastify'
 import { mealsRoutes } from './routes/meals.routes'
 import { usersRoutes } from './routes/users.routes'
 
 export const app = fastify()
+
+app.register(fastifyCors, {
+  origin: true,
+  credentials: true,
+})
 
 app.register(cookie)
 
